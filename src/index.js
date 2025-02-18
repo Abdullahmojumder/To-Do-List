016677adcb53ff235b5abbd339f3a104e6b676ad
Abdullahmojumder <abdullah.mojumder@gmail.com>
2025-02-18 11:29:54 +0600

    To-Do List Project Done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import { blankProjectLoad } from "./blank-projeect-load";
-import { initialdomManip } from "./initial-dom-manip";
 import { createToDo } from "./create-to-do";
+import { blankProjectLoad } from "./blank-projeect-load";
+import { displayDefaultProject, displayTheForm, addItemToCheckList, clearForm, displayToDo } from "./initial-dom-manip";
+import './style.css';
 
+// Call blankProjectLoad on first land
+//blankProjectLoad();
 
-//Call blankProjectLoad on first land
-blankProjectLoad();
+// Call Dom Manipulation module to control UI
+displayDefaultProject();
 
-//Call Dom Manipulation module to control UI
-initialdomManip();
+// Click events module
+let clickEventsModule = (function() {
 
-//TODO: click event module here for todo and project creations
+    // Click event for displaying the form
+    const addNewToDo = document.querySelector(".add-todo-button");
+    addNewToDo.addEventListener("click", displayTheForm);
 
-//Call create-to-do.js module file and apply some objects/properties
-const myToDo = createToDo("Grocery Run", "Go get groceries", "6/15/2025", "Low", "Milk, Eggs, Bread");
-const myToDo2 = createToDo("Homework", "Do Odin Project Work", "6/20/2025", "Medium", "Do ToDo List Project");
-console.log("Show me properties on myToDo from index.js ....", myToDo);
-console.log("Show me properties on myToDo2 from index.js ....", myToDo2);
+    // Click event for adding an item to the checklist on the form
+    const addToChecklist = document.querySelector(".add-to-checklist");
+    addToChecklist.addEventListener("click", addItemToCheckList);
+
+    // Click event to clear the form
+    const clearButton = document.querySelector(".reset-button");
+    clearButton.addEventListener("click", clearForm);
+
+    // Click event to submit a new todo form to project
+    const submitButton = document.querySelector(".submit-button");
+    submitButton.addEventListener("click", createToDo);
+    
+})();
